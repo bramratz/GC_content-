603edda946719b268f0a4bc0b0c950b4ec474ae1
Bram Ratz <bratz@uoguelph.ca>
2020-06-08 15:59:27 -0400

    Basic python program to calculate the GC content of each sequence as identified by a sequence ID in the file. ID and its GC content are saved in a dictionary and printed to the terminal once the file has dinished being read.

---
+++ computeGCcontentFasta.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Given a file in Fasta format, computes GC content as a 
+percentage for each sequence in the file.
+ID with GC percentage are printed out to the terminal
+
+Created on Mon Jun  8 15:57:14 2020
+
+@author: bram
+"""
+
